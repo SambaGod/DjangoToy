09af772e92f908948eadcc9a23c37d8ebecb25a1
Waleed <waleed.gatosorriso@gmail.com>
2021-06-14 19:07:51 +0200

    refined url paths

--- django_toy/meetups/urls.py
+++ django_toy/meetups/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 # urlpatterns needs to be named exactly this way so Django can find it
 urlpatterns = [
-    path('meetups/', views.index), # if our-domain/meetups is requested, fire index view
-    path('meetups/<slug:meetup_slug>', views.meetup_details )
+    path('meetups/', views.index, name='all-meetups'), # if our-domain/meetups is requested, fire index view
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail' )
 ]
