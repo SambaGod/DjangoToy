12356efe5a825387147af585783c7d1580890ef4
Waleed <waleed.gatosorriso@gmail.com>
2021-06-16 19:59:21 +0200

    optimized urls

--- django_toy/meetups/urls.py
+++ django_toy/meetups/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 # urlpatterns needs to be named exactly this way so Django can find it
 urlpatterns = [
-    path('meetups/', views.index, name='all-meetups'), # if our-domain/meetups is requested, fire index view
-    path('meetups/<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
-    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail' )
+    path('', views.index, name='all-meetups'), # if our-domain/meetups is requested, fire index view
+    path('<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
+    path('<slug:meetup_slug>', views.meetup_details, name='meetup-detail' )
 ]
