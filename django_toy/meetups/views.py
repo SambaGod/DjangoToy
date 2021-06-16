4d7d8f2fb18ac07567fb83e3cd25785158ad0f67
Waleed <waleed.gatosorriso@gmail.com>
2021-06-16 19:03:17 +0200

    Added registration form

--- django_toy/meetups/views.py
+++ django_toy/meetups/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Meetup
+from .forms import RegistrationForm
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -15,12 +16,21 @@ def index(request):
 def meetup_details(request, meetup_slug):
     print('Slug:', meetup_slug)
     try:
-        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        selected_meetup = Meetup.objects.get(slug=meetup_slug) 
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetup.participants.add(participant)  
+                 
         return render(request, 'meetups/meetup_details.html', {
-            # 'meetup_title': selected_meetup['title'], # In case of static data
-            'meetup_found': True,
-            # 'meetup_title': selected_meetup.title,
-            'meetup': selected_meetup,
+        # 'meetup_title': selected_meetup['title'], # In case of static data
+        'meetup_found': True,
+        # 'meetup_title': selected_meetup.title,
+        'meetup': selected_meetup,
+        'form': registration_form
         })
     except Exception as exc:
         return render(request, 'meetups/meetup_details.html', {
